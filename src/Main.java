18359d9130c500bdc57fe471d9b5e5845eeacfe1
Jenek <Jenek619@gmail.com>
2023-06-07 14:28:27 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int amount = 12000;
+        int bonus = 20;
+
+        System.out.println("Бонусные мили начислины: " + amount / bonus);
+    }
+}
